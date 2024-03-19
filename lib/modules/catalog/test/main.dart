e8e68c3d1bdb2a022c5f7164fe437456cc29f9cd
marlonespelho <marlon.b.e.mbe@gmail.com>
2024-03-19 00:10:31 -0300

    test: add catalog repositories tests

--- lib/modules/catalog/test/main.dart
+++ lib/modules/catalog/test/main.dart
@@ -1,5 +1,7 @@
 import 'store/main.dart' as storeTests;
+import 'repository/main.dart' as repositoryTests;
 
 void main() {
   storeTests.main();
+  repositoryTests.main();
 }
---
+++ lib/modules/catalog/test/repository/main.dart
@@ -0,0 +1,7 @@
+import 'credit_repository_test.dart' as creditRepositoryTest;
+import 'movie_repository_test.dart' as movieRepositoryTest;
+
+void main() {
+  creditRepositoryTest.main() ;
+  movieRepositoryTest.main();
+}
