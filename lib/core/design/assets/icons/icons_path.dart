86a46b64470b806f18cf27b826fdfbb5c0318871
marlonespelho <marlon.b.e.mbe@gmail.com>
2024-03-19 10:05:34 -0300

    fix: add icon files assets

---
+++ lib/core/design/assets/icons/icons_path.dart
@@ -0,0 +1,5 @@
+class IconsPath {
+  static const String iconLogo = 'lib/core/design/assets/icons/logo.svg';
+  static const String iconSearch = 'lib/core/design/assets/icons/search.svg';
+  static const String iconMedalStar = 'lib/core/design/assets/icons/medal_star.svg';
+}
